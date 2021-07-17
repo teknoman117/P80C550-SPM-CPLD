2390c28be2a3f90cc3c265ca2658c425dfe74416
Nathaniel R. Lewis <linux.robotdude@gmail.com>
2021-07-17 15:54:49 -0700

    first commit

---
+++ compile.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export WINEPREFIX="$HOME/.wine32"
+export WINEARCH=win32
+
+die() {
+    echo "command failure"
+    exit 1
+}
+
+wine "c:\\Wincupl\\Shared\\cupl.exe" -uabm4kb "c:\\Wincupl\\Shared\\cupl.dl" SELFPROGRAMBOARD.PLD || die
+
+wine "c:\\Wincupl\\WinCupl\\Fitters\\fit1504.exe" SELFPROGRAMBOARD.tt2 -device P1504C44 -preassign keep -strategy JTAG = off -strategy pin_keep off -strategy output_fast on || die
