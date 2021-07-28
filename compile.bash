c59fd3f621ee31ee33f446b5587244da134dd10b
Nathaniel R. Lewis <linux.robotdude@gmail.com>
2021-07-27 21:50:33 -0700

    improved compile script

--- compile.bash
+++ compile.bash
@@ -8,12 +8,17 @@ die() {
     exit 1
 }
 
-wine "c:\\Wincupl\\Shared\\cupl.exe" -uabm4kb "c:\\Wincupl\\Shared\\cupl.dl" SELFPROGRAMBOARD.PLD || die
-
-wine "c:\\Wincupl\\WinCupl\\Fitters\\fit1504.exe" \
-    SELFPROGRAMBOARD.tt2 \
-    -device P1504C44 \
-    -preassign keep \
-    -strategy JTAG = off \
-    -strategy pin_keep off \
-    -strategy output_fast on || die
+if [ "${1}" = "-s" ]; then
+    wine "c:\\Wincupl\\Shared\\cupl.exe" \
+        -uabm1s "c:\\Wincupl\\Shared\\cupl.dl" SELFPROGRAMBOARD.pld || die
+else
+    wine "c:\\Wincupl\\Shared\\cupl.exe" \
+        -uabm4kb "c:\\Wincupl\\Shared\\cupl.dl" SELFPROGRAMBOARD.pld || die
+    wine "c:\\Wincupl\\WinCupl\\Fitters\\fit1504.exe" \
+        SELFPROGRAMBOARD.tt2 \
+        -device P1504C44 \
+        -preassign keep \
+        -strategy JTAG = off \
+        -strategy pin_keep off \
+        -strategy output_fast on || die
+fi
