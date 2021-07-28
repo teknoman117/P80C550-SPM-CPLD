47acd129122910d11afffeabd39023049d94d06b
Nathaniel R. Lewis <linux.robotdude@gmail.com>
2021-07-27 20:58:18 -0700

    fix up build files

--- compile.bash
+++ compile.bash
@@ -10,4 +10,10 @@ die() {
 
 wine "c:\\Wincupl\\Shared\\cupl.exe" -uabm4kb "c:\\Wincupl\\Shared\\cupl.dl" SELFPROGRAMBOARD.PLD || die
 
-wine "c:\\Wincupl\\WinCupl\\Fitters\\fit1504.exe" SELFPROGRAMBOARD.tt2 -device P1504C44 -preassign keep -strategy JTAG = off -strategy pin_keep off -strategy output_fast on || die
+wine "c:\\Wincupl\\WinCupl\\Fitters\\fit1504.exe" \
+    SELFPROGRAMBOARD.tt2 \
+    -device P1504C44 \
+    -preassign keep \
+    -strategy JTAG = off \
+    -strategy pin_keep off \
+    -strategy output_fast on || die
